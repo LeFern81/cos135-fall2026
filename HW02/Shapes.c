5f847310b0239cf70980d0c3c9ba374ea0f1ce08
LeFern81 <stellahardy35@gmail.com>
2026-09-24 10:59:52 -0400

    Created CreatePyramid
    made the function, the main function, and created reverse pyramid

--- HW02/Shapes.c
+++ HW02/Shapes.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void CreatePyramid(int x){
+
+    for(int i = 1; i <= x; i++){
+        int gap = x-i;
+        for(int j = 0; j <= gap; j++){
+            printf(" ");
+        }
+        for(int j = 0; j < i+(i-1); j++){
+            printf("*");
+
+        }
+
+        printf("\n");
+
+    }
+
+}
+
+
+
+
+
+int main(){
+
+    CreatePyramid(6);
+}
